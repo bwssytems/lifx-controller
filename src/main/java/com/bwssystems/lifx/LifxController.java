dc2be515c373432598dc18d1e11696d7af8d36f8
Admin <info@bwssystems.com>
2017-02-01 16:17:23 -0600

    Update test controller

---
+++ src/main/java/com/bwssystems/lifx/LifxController.java
@@ -0,0 +1,42 @@
+package com.bwssystems.lifx;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.besherman.lifx.LFXClient;
+import com.github.besherman.lifx.LFXLight;
+
+public class LifxController {
+    private static final Logger log = LoggerFactory.getLogger(LifxController.class);
+
+    public static void main(String[] args) {
+    	LFXClient client = new LFXClient();        
+    	try {
+    		log.info("Open Lifx client....");
+			client.open(true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	try {
+    		log.info("List Lifx lights....");
+    	    for(LFXLight light: client.getLights()) {
+    	        System.out.format("Light: %n");
+    	        System.out.format("\tid=%s%n", light.getID());
+    	        System.out.format("\tlabel=%s%n", light.getLabel());
+    	        System.out.format("\tpower=%s%n", light.isPower());
+    	        System.out.format("\ttime=%s%n", light.getTime());
+    	        System.out.format("\tcolor=%s%n", light.getColor());                
+    	    }
+    	} finally {
+    	    client.close();
+    	}
+		log.info("Finished.");
+
+    }
+}
